a864f5b6726df87dec609df743e4d168847a045a
unknown <sokda106@gmail.com>
2024-10-12 23:21:37 +0700

    final code

---
+++ BONUS-2.dart
@@ -0,0 +1,38 @@
+
+
+void main(List<String> args) {
+  isBalance('{what is (42)}');
+  isBalance('[text}');
+  isBalance('{[[(a)b]c]d}');
+}
+
+void isBalance (String input) {
+  final splitInput = input.split('');
+  List<String> stack =  [];
+  var marching = {
+    ')':'(',
+    '}':'{',
+    ']':'['
+  };
+  for(var i = 0;i<splitInput.length;i++){
+    if(marching.containsValue(splitInput[i])){
+      // add only String that have in marching list
+      // other do not add 
+      stack.add(splitInput[i]);  
+      
+    } else if(marching.containsKey(splitInput[i])){
+      
+      if(stack.removeLast() != marching[splitInput[i]]){  
+         // the first stack.removeLast is [ 
+         //and marching[]] = [ 
+         //so it equal, make it Balance
+        
+        print('Not Balance');
+          return;
+      }
+      
+    } 
+  }
+  print('Balance');
+  
+}
