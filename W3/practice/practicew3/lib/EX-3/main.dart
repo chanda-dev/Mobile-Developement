43ea9d493290dad8b7bad7b1a05632211a7b0cf9
unknown <sokda106@gmail.com>
2024-10-27 17:42:39 +0700

    final

--- W3/practice/practicew3/lib/EX-3/main.dart
+++ W3/practice/practicew3/lib/EX-3/main.dart
@@ -1,45 +1,121 @@
 import 'package:flutter/material.dart';
-class ConlumbGradient extends StatelessWidget {
-final  String? text;
-final Color? color;
-final Gradient? gradient;
-const ConlumbGradient({super.key,this.text,this.gradient,this.color});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 20),
-      decoration: BoxDecoration(
-        gradient: gradient,
-        color: color,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Center(
-        child: Text(
-          '$text',
-          style: const TextStyle(
-            color: Colors.white,
-            decoration: TextDecoration.none
-
-          ),
-        ),
-      )
-    );
-  }
-}
 void main() {
   runApp(MaterialApp(
     home: Container(
       color: const Color(0xffE0E0E0),
       padding: const EdgeInsets.all(20),
-      child:const Column(
-        children: [
-                  ConlumbGradient(text: 'OOP',color: Color(0xffBBDEFB),),
-                  ConlumbGradient(text: 'DART',color: Color(0xff64B5F6),),
-                  ConlumbGradient(text: 'OOP',gradient: LinearGradient(colors: [Color(0xff4EA7F1),Color(0xff2B1975)]),),
-                  
+      child: Column(
+        children: [ 
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: const Color(0xffBBDEFB),
+            ),
+            child: const Center(
+              child: Text(
+                'OOP',
+                style: TextStyle(
+                  decoration: TextDecoration.none,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: const Color(0xff64B5F6),
+            ),
+            child: const Center(
+              child: Text(
+                'DART',
+                style: TextStyle(
+                  decoration: TextDecoration.none,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: const Color(0xff1E88E5),
+            ),
+            child: const Center(
+              child: Text(
+                'FLUTTER',
+                style: TextStyle(
+                  decoration: TextDecoration.none,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          const Text(
+            'BOUNCE',
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              color: Colors.black,
+              
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: const Color(0xffBBDEFB),
+            ),
+            child: const Center(
+              child: Text(
+                'OOP',
+                style: TextStyle(
+                  decoration: TextDecoration.none,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: const Color(0xff64B5F6),
+            ),
+            child: const Center(
+              child: Text(
+                'DART',
+                style: TextStyle(
+                  decoration: TextDecoration.none,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: const LinearGradient(colors: [Color(0xff4EA7F1),Color(0xff2B1975)])
+            ),
+            
+            child: const Center(
+              child: Text(
+                'FLUTTER',
+                style: TextStyle(
+                  decoration: TextDecoration.none,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          
+          
         ],
-      ) 
+      ),
      ),
   ));
 }
